9dbccd93e8fcb494aebcbd98e0a6ff7434aae929
melina_kozanitas <mkozanitas@gmail.com>
2021-11-19 22:11:25 -0800

    adding scipts and files to project for colab with the great T.W.O.
    all of these need to be cleaned or changes in some way be warned...

---
+++ Chi.square.R
@@ -0,0 +1,44 @@
+#clear workspace
+rm(list=ls())
+
+#set working directory
+setwd("~/R projects/SOD_ecology")
+
+#packages needed 
+install.packages("CGPfunctions")  ##graphs
+install.packages("lsr")  ##cramersV
+install.packages("dplyr")  ##subsetting 
+
+library(CGPfunctions)
+library(lsr)
+library(dplyr)
+
+
+bay.data <- read.csv("Bay.Final.csv")
+attach(bay.data) #allows R to find column titles
+
+#create data frame 
+Pram<-table (PLOT, RESULT.PEAK.12) # do this with each time point to get totals
+ftable(Pram)
+summary(Pram)
+
+
+hotVcold <- read_csv("chi.2.hs.cs.csv")
+attach(hotVcold)
+x2hvc<- table (HS,total.pram)
+ftable(x2hvc)
+summary(x2hvc)
+cramersV(x2hvc)
+margin.table(x2hvc,1)
+margin.table(x2hvc,2)
+prop.table(x2hvc)
+prop.table(x2hvc,1)
+prop.table(x2hvc,2)
+
+chi2.all <- read_csv("chi2.all.csv")
+attach(chi2.all)
+chi2.allc<- table (pram.total,status)
+ftable(chi2.all)
+summary(chi2.all)
+
+chi2.allc<- table (status, pram.total)
