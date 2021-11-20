a133031da09786c49c3bcf37c0e5956f4f698cca
melina_kozanitas <mkozanitas@gmail.com>
2021-11-19 23:24:38 -0800

    slight update to script for BA/ Density Vs Hot/Cold plots

--- BA.Density.hot.cold.R
+++ BA.Density.hot.cold.R
@@ -1,9 +1,13 @@
-fake_density_data <- data.frame(density = c(50, 79, 81, 54, 76, 64, 85, 76, 70, 66, 50, 40, 45, 66, 56, 54, 60, 70, 51, 33), type = c(rep("HS", 10), rep("CS", 10)))
+#Basal Area and Density of Bay in Hotplots Vs Coldplots
 
-fake_density_data
-hot <- fake_density_data[fake_density_data$type == "HS",]
-cold <- fake_density_data[fake_density_data$type == "CS",]
-t.test(hot$density, cold$density)
+
+#some super helpful fake data from Todd - for running a two-sample unpaired t-test
+#fake_density_data <- data.frame(density = c(50, 79, 81, 54, 76, 64, 85, 76, 70, 66, 50, 40, 45, 66, 56, 54, 60, 70, 51, 33), type = c(rep("HS", 10), rep("CS", 10)))
+
+#fake_density_data
+#hot <- fake_density_data[fake_density_data$type == "HS",]
+#cold <- fake_density_data[fake_density_data$type == "CS",]
+#t.test(hot$density, cold$density)
 
 
 BA.hs.cs <- data.frame(basal.area = c(5.991124528, 1.626441787, 7.319918737, 0.147699361, 16.29587045, 3.820641361, 6.789358716, 2.377722254, 10.73237689, 17.3610667, 0.501023814, 11.3512065, 10.08890279, 14.361832, 15.63041567), type = c(rep("CS", 8), rep("HS", 7)))
